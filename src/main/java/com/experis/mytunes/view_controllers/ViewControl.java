2d553a1c554037ec08aeb6076eb65db812fac56a
CasaRol <32219634+CasaRol@users.noreply.github.com>
2021-02-08 14:12:09 +0100

    Feature <song search>: Added new view for search with no result

--- src/main/java/com/experis/mytunes/view_controllers/ViewControl.java
+++ src/main/java/com/experis/mytunes/view_controllers/ViewControl.java
@@ -2,11 +2,14 @@ package com.experis.mytunes.view_controllers;
 
 import com.experis.mytunes.dataAccess.RandomDataHandler;
 import com.experis.mytunes.dataAccess.SearchDataHandler;
+import com.experis.mytunes.models.SearchResult;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
 
 @Controller
 public class ViewControl {
@@ -25,7 +28,11 @@ public class ViewControl {
 
     @RequestMapping(value = "/search_song", method = RequestMethod.GET)
     public String getSearchedSong(Model model, @RequestParam(value="searchTerm") String search) {
-        model.addAttribute("result", searchData.getSearch(search).get(0));
+        ArrayList<SearchResult> searchResults = searchData.getSearch((search));
+        if(searchResults.size() <= 0) {
+            return "Error404";
+        }
+        model.addAttribute("result", searchResults.get(0));
         //model.addAttribute("result", search);
         return "search_result";
 
